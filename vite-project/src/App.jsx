f71fccc1647e4175e269e04572cc9510b7f2369a
mbortg995 <mbortg995@gmail.com>
2025-03-04 10:06:19 +0100

    feat: authcontext provider to the solution + visual improvements on the login page.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import LoginPage from '@/pages/auth/LoginPage.jsx'
 import RegisterPage from '@/pages/auth/RegisterPage.jsx'
+import { AuthProvider } from './context/AuthContext'
 
 
 
@@ -9,10 +10,13 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<h1>LOGUEADO</h1>}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
---
+++ vite-project/src/context/AuthContext.jsx
@@ -0,0 +1,65 @@
+import { createContext, useContext, useState } from "react"
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [error, setError] = useState("");
+
+  const token = localStorage.getItem("token");
+  const isAutenticated = token !== null;
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  const login = async (data) => {
+    try {
+      const response = await fetch('http://localhost:3000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        setError(error.error);
+        return false;
+      }
+
+      const { user, token } = await response.json();
+
+      localStorage.setItem('user', JSON.stringify({
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      }));
+
+      localStorage.setItem('token', token);
+
+      return true;
+
+    } catch {
+      setError("Servicio no disponible. Inténtelo de nuevo más adelante.");
+      return false;
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      token,
+      isAutenticated,
+      login,
+      error
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  return context;
+}
